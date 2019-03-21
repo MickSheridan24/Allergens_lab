dd927853652a2520d7a6f3de91843ebaa3f75c49
Mickey <micksheridan.24@gmail.com>
2019-03-21 14:15:46 -0400

    Migrations

---
+++ db/migrate/20190321181330_create_allergens.rb
@@ -0,0 +1,11 @@
+class CreateAllergens < ActiveRecord::Migration[5.2]
+  def change
+    create_table :allergens do |t|
+      t.integer :user_id
+      t.integer :ingredient_id
+      t.integer :deadliness
+
+      t.timestamps
+    end
+  end
+end
