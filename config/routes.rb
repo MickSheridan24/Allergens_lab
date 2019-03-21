d2c4334945f9f744704aab929019e8e0d3f29569
Mickey <micksheridan.24@gmail.com>
2019-03-21 18:02:34 -0400

    current

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :ingredients, only: [:show, :index]
+  resources :recipes, only: [:index, :show, :new, :create]
+  get "users/:id", to: "users#show", as: "user"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create(name: "Gordon Ramsay")
+user2 = User.create(name: "Billy McCantTouchNuts")
+
+recipe = Recipe.create(name: "Peruvian Chicken", description: "It's divine!", :user_id => user1.id)
+recipe2 = Recipe.create(name: "Chocolate Something, Wow!", description: "You can tell where my head is at right now", :user_id => user1.id)
+
+chicken = Ingredient.create(name: "chicken")
+chocolate = Ingredient.create(name: "chocolate")
+salt = Ingredient.create(name: "salt")
+milk = Ingredient.create(name: "milk ")
+egg = Ingredient.create(name: "egg")
+peanut = Ingredient.create(name: "peanut")
+
+recipe.ingredients << chicken
+recipe.ingredients << salt
+recipe2.ingredients << chocolate
+recipe2.ingredients << milk
+recipe2.ingredients << egg
+recipe2.ingredients << peanut
+
+Allergen.create(deadliness: 10000, :user_id => user2.id, :ingredient_id => peanut.id)
